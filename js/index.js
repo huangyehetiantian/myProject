633a4532162eed6cf94359577430fae50975b04f
yandong <282384585@qq.com>
2017-03-21 14:22:34 +0800

    提交项目初始步骤

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+document.write(11111);
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+module.exports={
+    entry:"./entry.js",
+    output:{
+        path:"./build",
+        filename:"build.js"
+    }
+}
